3873e1ed2e6d7cdd12c28d9ddf0ccd3523447c81
Theis <thph@itu.dk>
2024-02-26 10:44:15 +0100

    feat: Create main.go (hello world) Driver:Jonas
    Co-authored-by: Haruyzal <racheeeve@gmail.com>
    Co-authored-by: JonasSkjodt <jonasmeister@gmail.com>
    Co-authored-by: August-Brandt <augustbrandt170@gmail.com>
    Co-authored-by: zqueamish <apno@itu.dk>
    Co-authored-by: Alex-itu <lolk@itu.dk>

--- src/main.go
+++ src/main.go
@@ -1,10 +1,13 @@
 package main
 
 import(
-
-
+	"flag"
+	"fmt"
 )
 
 func main() {
+	name := flag.String("name", "world", "This takes a name/string ")
+	flag.Parse()
 
+	fmt.Printf("Hello, %s\n", *name)
 }

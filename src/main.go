79f4d6914c23905e38ee8b24b67ca858d352ee32
Theis <thph@itu.dk>
2024-02-26 14:34:33 +0100

    fix: fix the import in main.go to work Driver: August
    Co-authored-by: Haruyzal <racheeeve@gmail.com>
    Co-authored-by: August-Brandt <augustbrandt170@gmail.com>
    Co-authored-by: Alex-itu <lolk@itu.dk>
    Co-authored-by: JonasSkjodt <jonasmeister@gmail.com>

--- src/main.go
+++ src/main.go
@@ -3,12 +3,12 @@ package main
 import(
 	"flag"
 	"fmt"
-	"github.com/Agile-Arch-Angels/decentralized-tech-radar_dev"
+	"github.com/Agile-Arch-Angels/decentralized-tech-radar_dev/src/SayHello"
 )
 
 func main() {
 	name := flag.String("name", "world", "This takes a name/string ")
 	flag.Parse()
 
-	fmt.Println(SayHello(*name))
+	fmt.Println(SayHello.SayHello(*name))
 }

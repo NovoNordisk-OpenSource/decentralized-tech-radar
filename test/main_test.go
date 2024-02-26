91e292360d8afc5fb7e8b8f7b0c17ceaac8244ff
Theis <thph@itu.dk>
2024-02-26 15:02:05 +0100

    test: wrote the test for main.go Driver: Annabell
    Co-authored-by: Haruyzal <racheeeve@gmail.com>
    Co-authored-by: August-Brandt <augustbrandt170@gmail.com>
    Co-authored-by: Alex-itu <lolk@itu.dk>
    Co-authored-by: JonasSkjodt <jonasmeister@gmail.com>
    Co-authored-by: zqueamish <apno@itu.dk>

--- test/main_test.go
+++ test/main_test.go
@@ -1,10 +1,14 @@
 package test
 
 import (
+	"fmt"
+	"os/exec"
 	"testing"
+
 	"github.com/Agile-Arch-Angels/decentralized-tech-radar_dev/src/SayHello"
 )
 
+// unit test
 func TestSayHello(t *testing.T) {
 	output := SayHello.SayHello("mate")
 	if output != "Hello, mate" {
@@ -12,4 +16,15 @@ func TestSayHello(t *testing.T) {
 	}
 }
 
+// E2E test
+/*func TestMain(t *testing.T) {
+	name := "Naanaualjoth"
+	cmd := exec.Command("cd", "../dist", "&&", "./main", "-name", name)
 
+	cmd_output, err := cmd.Output()
+	if err != nil {
+		t.Errorf("%v", err)
+	} else if string(cmd_output) != (fmt.Sprintf("Hello, %s", name)) {
+		t.Errorf("Output didn't match expected. %s", string(cmd_output))
+	}
+}*/

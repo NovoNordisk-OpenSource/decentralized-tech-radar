1c3b4b3b171803a74ff6d1754c60b00f40554587
JonasSkjodt1 <skjo@itu.dk>
2024-03-04 17:54:18 +0100

    chore: Create index file and write greeting to file
    Co-authored-by: Alex-itu <lolk@itu.dk>

--- src/main.go
+++ src/main.go
@@ -1,8 +1,9 @@
 package main
 
-import(
+import (
 	"flag"
 	"fmt"
+	"os"
 	"github.com/Agile-Arch-Angels/decentralized-tech-radar_dev/src/SayHello"
 )
 
@@ -10,5 +11,20 @@ func main() {
 	name := flag.String("name", "world", "This takes a name/string ")
 	flag.Parse()
 
-	fmt.Println(SayHello.SayHello(*name))
+	greeting := SayHello.SayHello(*name)
+
+	// Open index.html for writing (create if it doesn't exist)
+	file, err := os.OpenFile("index.html", os.O_WRONLY|os.O_CREATE, 0644) // 0644 grants the owner read and write access
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Write the greeting as plain text to the file
+	_, err = file.WriteString(greeting + "\n")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Wrote to index.html")
 }

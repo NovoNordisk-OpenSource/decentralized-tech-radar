33c175e85440393d29b1b7c96d1b5c5998d0e520
Theis <thph@itu.dk>
2024-03-05 12:50:36 +0100

    cd: initial dagger flow setup. Moved files from my cocommit repo
    Co-authored-by: zqueamish <apno@itu.dk>
    Co-authored-by: Haruyzal <racheeeve@gmail.com>

---
+++ ci/build_test_release.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	goCache := client.CacheVolume("golang")
+
+	// use a node:16-slim container
+	// mount the source code directory on the host
+	// at /src in the container
+	source := client.Container().
+		From("golang:1.21").
+		WithDirectory("/src", client.Host().Directory(".", dagger.HostDirectoryOpts{
+			Exclude: []string{"ci/"},
+		})).WithMountedCache("/src/dagger_dep_cache/go_dep", goCache)
+
+	geese := []string{"darwin", "linux", "windows"}
+	goarch := "amd64"
+
+	// set the working directory in the container
+	// install application dependencies
+	runner := source.WithWorkdir("/src").
+		WithExec([]string{"go", "mod", "tidy"})
+
+	// run application tests
+	test := runner.WithWorkdir("/src/src_code/go_src").WithExec([]string{"go", "test"})
+
+	buildDir := test.Directory("/src/")
+
+	for _, goos := range geese {
+		path := fmt.Sprintf("/dist/")
+		filename := fmt.Sprintf("/dist/tech_radar_%s", goos)
+		// build application
+		// write the build output to the host
+		build := test.
+			WithEnvVariable("GOOS", goos).
+			WithEnvVariable("GOARCH", goarch).
+			WithExec([]string{"go", "build", "-o", filename})
+
+		buildDir = buildDir.WithDirectory(path, build.Directory(path))
+
+	}
+
+	_, err = buildDir.Export(ctx, ".")
+	if err != nil {
+		panic(err)
+	}
+	e, err := buildDir.Entries(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("build dir contents:\n %s\n", e)
+}
---
+++ ci/test_on_push.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	goCache := client.CacheVolume("golang")
+
+	// use a node:16-slim container
+	// mount the source code directory on the host
+	// at /src in the container
+	source := client.Container().
+		From("golang:1.21").
+		WithDirectory("/d_src", client.Host().Directory(".", dagger.HostDirectoryOpts{
+			Exclude: []string{},
+		})).WithMountedCache("/d_src/ci/cache", goCache)
+
+		// set the working directory in the container
+		// install application dependencies
+	runner := source.WithWorkdir("/d_src/src").
+		WithExec([]string{"go", "mod", "tidy"})
+
+		// run application tests
+	out, err := runner.WithWorkdir("/d_src/test").WithExec([]string{"go", "test"}).
+		Stderr(ctx)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(out)
+}

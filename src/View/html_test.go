5fbbca3eb2160fd7d2a79238c000430a5759d898
JonasSkjodt1 <skjo@itu.dk>
2024-03-12 10:28:15 +0100

    feat: add test and refactored main
    Co-authored-by: Alex-itu <lolk@itu.dk>
    Co-authored-by: August-Brandt <augustbrandt170@gmail.com>
    Co-authored-by: zqueamish <apno@itu.dk>

--- src/View/html_test.go
+++ src/View/html_test.go
@@ -1,5 +1,53 @@
 package view
 
 import (
+	"os"
+	"strings"
+	"testing"
+
+	Reader "github.com/Agile-Arch-Angels/decentralized-tech-radar_dev/src/specReader"
 )
 
+func TestGenerateHtml(t *testing.T) {
+	//set up data
+	blips := Reader.Blips{
+		Blips: []Reader.Blip{
+			{
+				Name:        "Test name",
+				Quadrant:    "Test quadrant",
+				Ring:        "Test ring",
+				IsNew:       true,
+				Moved:       0,
+				Description: "Test description",
+			},
+		},
+	}
+
+	//Generate the HTML file
+	GenerateHtml(blips)
+
+	//check if the index.html was created
+	_, err := os.Stat(htmlFileName + ".html")
+	if os.IsNotExist(err) {
+		t.Fatalf("Exptected HTML was not created.")
+	}
+
+	//read content of the HTML file
+	content, err := os.ReadFile(htmlFileName + ".html")
+	if err != nil {
+		t.Fatalf("Could not read the generated HTML file: %v", err)
+	}
+	
+	//convert content to string and check if contains the test data
+	contentStr := string(content)
+	if !strings.Contains(contentStr, "Test name") ||
+	!strings.Contains(contentStr, "Test quadrant") {
+		t.Errorf("HTML doesnt contain the expected data.")
+	}
+
+	//clean up test after test is completed
+	err = os.Remove(htmlFileName + ".html")
+	if err != nil {
+		t.Fatalf("Failed to remove test HTML file: %v", err)
+	}
+}

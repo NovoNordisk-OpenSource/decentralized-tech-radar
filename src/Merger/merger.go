9ed1f70a44bfc7e1bbfc272d92276f6ce6c3a573
August-Brandt <aubr@itu.dk>
2024-03-21 15:10:31 +0100

    fix: Add newline between merged files, and delete merged_file if one exists before merging

--- src/Merger/merger.go
+++ src/Merger/merger.go
@@ -6,12 +6,14 @@ import (
 )
 
 func MergeCSV(file1 string, file2 string) {
+	os.Remove("Merged_file.csv")
 	var buf bytes.Buffer
 	b1, err := os.ReadFile(file1)
 	if err != nil {
 		panic(err)
 	}
 	buf.Write(b1)
+	buf.Write([]byte("\n"))
 	b2, err := os.ReadFile(file2)
 	if err != nil {
 		panic(err)

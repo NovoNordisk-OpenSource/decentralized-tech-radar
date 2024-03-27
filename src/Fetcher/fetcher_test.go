a87c57b6ecdbb0f0ffc3cafba07a8e9698b0d529
Theis <thph@itu.dk>
2024-03-27 12:54:32 +0100

    test: add text compare to test for fetching csv file

--- src/Fetcher/fetcher_test.go
+++ src/Fetcher/fetcher_test.go
@@ -1,6 +1,7 @@
 package Fetcher
 
 import (
+	"bufio"
 	"os"
 	"testing"
 )
@@ -21,30 +22,50 @@ func TestFetchFilesInvalidArguments(t *testing.T) {
 }
 
 func TestFetchFilesValidArguments(t *testing.T) {
-    //TODO: Maybe this needs to be split into 2 tests
-    
+	//TODO: Maybe this needs to be split into 2 tests
+
 	// dev repo link and create specfile
 	url := "https://github.com/Agile-Arch-Angels/decentralized-tech-radar_dev.git"
 	//TODO: Change this to main once templates folder is on main
-    branch := "feat_git_fetcher"
-    data := []byte("examples/csv_templates/template.csv")
-    os.WriteFile("./specfile.txt",data,0644)
-    specFile := "specfile.txt"
+	branch := "feat_git_fetcher"
+	data := []byte("examples/csv_templates/template.csv")
+	os.WriteFile("./specfile.txt", data, 0644)
+	specFile := "specfile.txt"
 
-    err := FetchFiles(url,branch,specFile)
+	err := FetchFiles(url, branch, specFile)
 
-    if err != nil {
-        t.Errorf("FetchFiles returned an err %v", err)
+	if err != nil {
+		t.Errorf("FetchFiles returned an err %v", err)
+	}
+
+	_, err = os.Stat("./cache/template.csv")
+if os.IsNotExist(err) {
+    t.Errorf("File wasn't downloaded or wasn't moved correctly: %v", err)
+}
+
+file, err := os.Open("./cache/template.csv")
+if err != nil {
+    t.Errorf("Failed to open template.csv. %v", err.Error())
+}
+scanner := bufio.NewScanner(file)
+
+template_lines := []string{}
+for scanner.Scan() {
+    template_lines = append(template_lines, scanner.Text())
+}
+
+expected_lines := []string{"name,ring,quadrant,isNew,move,description",
+"Python,hold,language,false,0,Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+"web,hold,language,false,0,Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+"react,hold,language,false,0,Lorem ipsum dolor sit amet consectetur adipiscing elit."}
+
+for i := range expected_lines {
+    if !(expected_lines[i] == template_lines[i]) {
+        t.Errorf("Mismatch in downloaded file. Expected: %v \n Retrieved: %v",expected_lines[i], template_lines[i])
     }
+}
 
-    _, err = os.Stat("./cache/template.csv")
-   if os.IsNotExist(err) {
-        t.Errorf("File wasn't downloaded or wasn't moved correctly: %v",err)
-    } 
+	defer os.Remove("specfile.txt")
+	defer os.RemoveAll("./cache")
 
-    //TODO: Maybe add a test for contents of CSV file?
-
-    defer os.Remove("specfile.txt")
-    defer os.RemoveAll("./cache")
-
-}   
+}

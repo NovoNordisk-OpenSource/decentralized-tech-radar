68c81cb61d154cad27dbc0b0a43627d82e085d8a
Haruyzal <racheeeve@gmail.com>
2024-04-08 09:57:40 +0200

    chore: Fix import syntax.

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
-import (
-	"github.com/NovoNordisk-OpenSource/decentralized-tech-radar/cmd"
+import "github.com/NovoNordisk-OpenSource/decentralized-tech-radar/cmd"
 
 func main() {
 	cmd.Execute()

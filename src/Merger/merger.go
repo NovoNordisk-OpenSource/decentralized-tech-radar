f4675de387f398f12b779ea7042057be8b4bc5c7
August-Brandt <aubr@itu.dk>
2024-04-30 13:03:45 +0200

    feat: Merge specfiles with urls for repos
    Co-authored-by: Slug-Boi <theis.p.holm@gmail.com>

--- src/Merger/merger.go
+++ src/Merger/merger.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 	"slices"
 	"strings"
 
@@ -96,6 +97,10 @@ func MergeCSV(filepaths []string) error {
 func ReadCsvData(buffer *bytes.Buffer, filepaths ...string) error {
 	// Map functions as a set (name -> quadrant)
 	var set = make(map[string][]string)
+
+	// This is hacky, but don't worry about it :)
+	// Map [lineWithFirstUrl] -> map[uniqueUrls[1:]]nil
+	var blipRepos = make(map[string]map[string]byte)
 	for _, filepath := range filepaths {
 		file, err := os.Open(filepath)
 		if err != nil {
@@ -103,12 +108,14 @@ func ReadCsvData(buffer *bytes.Buffer, filepaths ...string) error {
 		}
 
 		defer file.Close()
-		scanFile(file, buffer, set)
+		scanFile(file, buffer, set, &blipRepos)
 	}
+	
+	bufferWriter(buffer, blipRepos)
 	return nil
 }
 
-func scanFile(file *os.File, buffer *bytes.Buffer, set map[string][]string) {
+func scanFile(file *os.File, buffer *bytes.Buffer, set map[string][]string, blipRepos *map[string]map[string]byte) {
 	scanner := bufio.NewScanner(file)
 
 	// Skip header
@@ -123,15 +130,53 @@ func scanFile(file *os.File, buffer *bytes.Buffer, set map[string][]string) {
 		if index != -1 {
 			name = line[:index]
 		}
-
-		duplicateRemoval(name, line, buffer, set)
+		
+		err := duplicateRemoval(name, line, set, *blipRepos)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
-func duplicateRemoval(name, line string, buffer *bytes.Buffer, set map[string][]string) error {
+
+
+// Regex:
+// <br>Repos:<br> (?<atag><a href=(?<url>(https:\/\/www.|http:\/\/www.|https:\/\/|http:\/\/)([-a-zA-Z0-9]{2,})(.[a-zA-Z0-9]{2,})(.[a-zA-Z0-9]{2,})?(\/[-a-zA-Z0-9_\/.]{2,}))>(?<repoName>[-a-zA-Z\d_.]+)<\/a>)/gm
+
+// Map [BlipName] -> map[uniqueUrls]nil
+func bufferWriter(buffer *bytes.Buffer, blips map[string]map[string]byte) error {
+	var sb strings.Builder
+	for line, intMap := range blips {
+		sb.WriteString(line)
+		for atag, _ := range intMap {
+			sb.WriteString("<br>")
+			sb.WriteString(atag)
+		}
+		sb.WriteRune('\n')
+	}
+
+	buffer.WriteString(sb.String())
+
+	return nil
+}
+
+var regexPattern *regexp.Regexp = nil
+var pattern = "<br>Repos:(<br><a href=((https://www.|http://www.|https://|http://)([-a-zA-Z0-9]{2,})(.[a-zA-Z0-9]{2,})(.[a-zA-Z0-9]{2,})?(/[-a-zA-Z0-9_/.]{2,}))>([-a-zA-Z\\d_.]+)</a>)+"
+
+
+func duplicateRemoval(name, line string, set map[string][]string, blipRepos map[string]map[string]byte) error {
 	//TODO: Unmarshal the json file (or some other file based solution) to get the alternative names
 	// Or just use a baked in str read line by line or combination
 	//os.Stat("./Dictionary/alt_names.txt")
+
+	
+	var err error
+	if regexPattern == nil {
+		regexPattern, err = regexp.Compile(pattern)
+		if err != nil {
+			panic(err)
+		}
+	}
 
 	real_name := name
 	if alt_names[name] != "" {
@@ -144,11 +189,29 @@ func duplicateRemoval(name, line string, buffer *bytes.Buffer, set map[string][]
 		quadrant := line[len(real_name)+1 : strings.IndexByte(line[len(real_name)+1:], ',')+len(real_name)+1]
 		if !(slices.Contains(set[name], quadrant)) {
 			set[name] = append(set[name], quadrant)
-			buffer.Write([]byte(line + "\n"))
+			splitLine := strings.Split(line, "<br>Repos:<br>")
+			blipRepos[splitLine[0] + "<br>Repos:"] = make(map[string]byte)
+			for _, repo := range strings.Split(splitLine[1], "<br>") {
+				blipRepos[splitLine[0] + "<br>Repos:"][repo] = 0
+			}
+
+		// This else is for items we have already seen
+		} else {
+			splitLine := strings.Split(line, "<br>Repos:<br>")
+			atags := strings.Split(splitLine[1], "<br>")
+			
+			for _, atag := range atags {
+				blipRepos[splitLine[0] + "<br>Repos:"][atag] = 0
+			}
 		}
 	} else {
 		set[name] = append(set[name], line[len(name)+1:strings.IndexByte(line[len(name)+1:], ',')+len(name)+1])
-		buffer.Write([]byte(line + "\n"))
+		// buffer.Write([]byte(line + "\n"))'
+		splitLine := strings.Split(line, "<br>Repos:<br>")
+		blipRepos[splitLine[0] + "<br>Repos:"] = make(map[string]byte)
+		for _, repo := range strings.Split(splitLine[1], "<br>") {
+			blipRepos[splitLine[0] + "<br>Repos:"][repo] = 0
+		}
 	}
 
 	return nil

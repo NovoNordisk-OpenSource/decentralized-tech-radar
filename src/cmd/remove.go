55f1d689e07b9d431e3834c3c5486cb3d11708e8
Haruyzal <racheeeve@gmail.com>
2024-04-30 10:10:43 +0200

    docs: Define how Remove can be used.

--- src/cmd/remove.go
+++ src/cmd/remove.go
@@ -2,7 +2,12 @@ package cmd
 
 import "github.com/spf13/cobra"
 
-var remCmd = &cobra.Command{}
+var remCmd = &cobra.Command{
+	Use:   "remove <path/to/csvfile> <name> <quadrant>",
+	Short: "Removes a line from a csv spec file",
+	Long: `Removes the specified tool's line from the given csv specification file. The csv file's header must be in the following format:
+		name,ring,quadrant,isNew,moved,description`,
+}
 
 func init() {
 	rootCmd.AddCommand(remCmd)

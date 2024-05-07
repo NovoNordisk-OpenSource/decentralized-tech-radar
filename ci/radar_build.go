308766a5b63ba296b1487f9abffc3c53e6c30d70
Theis <thph@itu.dk>
2024-05-07 14:28:51 +0200

    ci: create dagger workflow for generating a radar html file

---
+++ ci/radar_build.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	goCache := client.CacheVolume("golang")
+
+	// use a node:16-slim container
+	// mount the source code directory on the host
+	// at /src in the container
+	source := client.Container().
+		From("golang:1.21").
+		WithDirectory("/d_src", client.Host().Directory(".", dagger.HostDirectoryOpts{
+			Exclude: []string{},
+		})).WithMountedCache("/d_src/ci/cache", goCache)
+
+	// set the working directory in the container
+
+	runner := source.WithWorkdir("/d_src/")
+
+	// Run the fetcher using the binary
+	runner = runner.WithWorkdir("/d_src/").WithExec([]string{"./Tech_Radar-linux", "fetch","--repo-file=repos.txt", "--whitelist=whitelist.txt"})
+
+	// Run the merger using the binary
+	runner = runner.WithWorkdir("/d_src/").WithExec([]string{"./Tech_Radar-linux", "merge", "--cache"})
+
+	// Run the HTML generator using the binary
+	exporter := runner.WithWorkdir("/d_src/").WithExec([]string{"./Tech_Radar-linux", "generate", "Merged_file.csv"})
+
+	// Output the HTML file to the host
+	_, err = exporter.Export(ctx, "index.html")
+	if err != nil {
+		panic(err)
+	}
+
+}
